6829afeadbc01623f680cb2ce0c3c5f6e1698645
thanhdang210203 <thanhdang210203@gmail.com>
2022-01-11 20:23:28 +0700

    added jump but the rigidbody keeps pushing itself out of the character controller

--- Hard f-ing game/Assets/Scripts/Cube_movement.cs
+++ Hard f-ing game/Assets/Scripts/Cube_movement.cs
@@ -7,13 +7,26 @@ public class Cube_movement : MonoBehaviour
 
     public Transform cam;
     public CharacterController controller;
-
+    public Transform groundCheck;
+    public LayerMask groundMask;
     public float speed = 5f;
+    public float jump = 2f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-
+    public float gravity = -9.81f;
+    public float groundDistance = 0.2f;
+    private Vector3 velocity;
+    private bool isGrounded;
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -26,6 +39,14 @@ public class Cube_movement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jump * -2.0f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
