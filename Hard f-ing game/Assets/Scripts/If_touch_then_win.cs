f6ced6c68a3eaa2ae86e9f4b66969898bc0a3b13
thanhdang210203 <thanhdang210203@gmail.com>
2022-01-13 13:55:32 +0700

    okay

--- Hard f-ing game/Assets/Scripts/If_touch_then_win.cs
+++ Hard f-ing game/Assets/Scripts/If_touch_then_win.cs
@@ -11,9 +11,13 @@ public class If_touch_then_win : MonoBehaviour
     public AudioClip Noti;
     private string str = "PASS THROUGH THE MAZE";
     [SerializeField] private GameObject FloatingNoti;
+    private Animation Float_Noti;
+    //private bool playOnce = true;
+    //private bool playOnce2 = true; 
+    //private bool playOnce3 = true;
     void Update()
     {
-        
+        Float_Noti = gameObject.GetComponent<Animation>();
     }
     void OnCollisionEnter(Collision ObjectCollidedWith)
     {
@@ -22,6 +26,7 @@ public class If_touch_then_win : MonoBehaviour
 
             AudioSource.PlayClipAtPoint(Start, new Vector3(0, 0, 0));
             Debug.Log("Let's start!");
+           
         }
 
         else if (ObjectCollidedWith.collider.tag == "WinBlock")
@@ -37,14 +42,15 @@ public class If_touch_then_win : MonoBehaviour
             AudioSource.PlayClipAtPoint(Noti, new Vector3(0, 0, 0));
             Debug.Log("Line crossed");
             ShowNoti(str);
+          
         }
     }
-        void ShowNoti(string text)
+    void ShowNoti(string text)
+    {
+        if (FloatingNoti)
         {
-            if (FloatingNoti)
-            {
-                GameObject prefab = Instantiate(FloatingNoti, transform.position, Quaternion.identity);
-                prefab.GetComponentInChildren<TextMesh>().text = text;            
-            }
+            GameObject prefab = Instantiate(FloatingNoti, new Vector3(-7.1f, 15.9f, 24.97148f), Quaternion.identity);
+            prefab.GetComponentInChildren<TextMesh>().text = text;
         }
+    }
 }
